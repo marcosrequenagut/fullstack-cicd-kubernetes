85fbf07154b2efa1ba45d780ee91acf8359c819d
marcosrequenagut <marcosrequenagut@gmail.com>
2026-09-21 16:40:13 +0200

    feat: add frontend deployment to Kubernetes with dedicated Ingress

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,15 @@ import pandas as pd
 from fastapi import FastAPI
 
 from db_connection import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:8080"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -13,7 +20,7 @@ def read_root():
 
 @app.get("/order-items")
 def get_order_items_data():
-    query = "SELECT * FROM order_items LIMIT 10"
+    query = "SELECT * FROM order_items LIMIT 100"
 
     df = pd.read_sql(query, con=engine)
 
---
+++ frontend/src/App.tsx
@@ -0,0 +1,52 @@
+import {useState, useEffect} from 'react'
+
+interface OrderItem {
+  order_id: string
+  order_item_id: number
+  product_id: string
+  seller_id: string
+  shipping_limit_date: string
+  price: number
+  freight_value: number
+}
+
+function App() {
+  const[items, setItems] = useState<OrderItem[]>([])
+
+  useEffect(() => {
+    fetch('http://dev.miproyecto.local/api/order-items')
+    .then((response) => response.json())
+    .then((data) => setItems(data))
+  }, [])
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Order ID</th>
+          <th>Order_item_id</th>
+          <th>Product_id</th>
+          <th>Seller_id</th>
+          <th>Shipping_limit_date</th>
+          <th>Price</th>
+          <th>freight_value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item, index) => (
+          <tr key={index}>
+            <td>{item.order_id}</td>
+            <td>{item.order_item_id}</td>
+            <td>{item.product_id}</td>
+            <td>{item.seller_id}</td>
+            <td>{item.shipping_limit_date}</td>
+            <td>{item.price}</td>
+            <td>{item.freight_value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default App
